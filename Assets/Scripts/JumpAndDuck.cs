8d0700e1231444db774e382a9ae62de253871153
Arthur <oxenarthur@gmail.com>
2019-10-11 17:57:30 +0300

    fixed a bug in which player would be able to press z on game over screen

--- Assets/Scripts/JumpAndDuck.cs
+++ Assets/Scripts/JumpAndDuck.cs
@@ -1,111 +1,138 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class JumpAndDuck : MonoBehaviour {
-	public Level level = null;
-	public GameObject ground = null;
-	public Collider2D standingCollider = null;
-	public Collider2D duckingCollider = null;
-	public AudioSource jumpAudioSource = null;
-	public AudioClip jumpAudioClip = null;
-	private Animator animator;
-	private bool grounded = true;
-	private bool ducking = false;
-	private float jumpVelocity = 0f;
-	private float gravity = 144f;
-	private Vector3 startVector;
-	public bool inverted = false;
+public class JumpAndDuck : MonoBehaviour
+{
+    public Level level = null;
+    public GameObject ground = null;
+    public Collider2D standingCollider = null;
+    public Collider2D duckingCollider = null;
+    public AudioSource jumpAudioSource = null;
+    public AudioClip jumpAudioClip = null;
+    private Animator animator;
+    private bool grounded = true;
+    private bool ducking = false;
+    private float jumpVelocity = 0f;
+    private float gravity = 144f;
+    private Vector3 startVector;
+    public bool inverted = false;
 
-	void Start() {
-		animator = GetComponent<Animator>();
-		standingCollider.enabled = true;
-		duckingCollider.enabled = false;
-	}
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        standingCollider.enabled = true;
+        duckingCollider.enabled = false;
+    }
 
-	void Update() {
-		if (grounded) {
-			if (Input.GetButton("Jump") || Input.GetAxis("Vertical") > 0) {
-				jump();
-			} else if (Input.GetAxis("Vertical") < 0) {
-				duck();
-			} else {
-				stand();
-			}
-		} else {
-			transform.position += jumpVelocity * Vector3.up * Time.deltaTime;
-			jumpVelocity -= gravity * Time.deltaTime;
+    void Update()
+    {
+        if (grounded)
+        {
+            if (Input.GetButton("Jump") || Input.GetAxis("Vertical") > 0)
+            {
+                jump();
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                duck();
+            }
+            else
+            {
+                stand();
+            }
+        }
+        else
+        {
+            transform.position += jumpVelocity * Vector3.up * Time.deltaTime;
+            jumpVelocity -= gravity * Time.deltaTime;
 
-			if (transform.position.y < ground.transform.position.y) {
-				grounded = true;
-				transform.position = startVector;
-				animator.SetBool("jumping", false);
-			} else if (3 < transform.position.y && 20 < jumpVelocity) {
-				jumpVelocity = 20;
-			}
-		}
-		if (Input.GetButtonDown("Invert")) {
-			inverted = !inverted;
-			invert();
-		}
-	}
+            if (transform.position.y < ground.transform.position.y)
+            {
+                grounded = true;
+                transform.position = startVector;
+                animator.SetBool("jumping", false);
+            }
+            else if (3 < transform.position.y && 20 < jumpVelocity)
+            {
+                jumpVelocity = 20;
+            }
+        }
+        if (Input.GetButtonDown("Invert") && !HitObstacles.stop)
+        {
+            inverted = !inverted;
+            invert();
+        }
+    }
 
-	void OnCollisionEnter2D(Collision2D collision) {
-		if (collision.gameObject == ground) {
-			grounded = true;
-			transform.position = startVector;
-			animator.SetBool("jumping", false);
-		}
-	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == ground)
+        {
+            grounded = true;
+            transform.position = startVector;
+            animator.SetBool("jumping", false);
+        }
+    }
 
 
-	void OnCollisionExit2D(Collision2D collision) {
-		if (collision.gameObject == ground) {
-			grounded = false;
-			animator.SetBool("jumping", true);
-		}
-	}
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject == ground)
+        {
+            grounded = false;
+            animator.SetBool("jumping", true);
+        }
+    }
 
-	void jump() {
-		if (!grounded) {
-			return;
-		}
-		stand();
-		if (jumpAudioSource && jumpAudioClip) {
-			jumpAudioSource.PlayOneShot(jumpAudioClip, 1);
-		}
-		startVector = transform.position;
-		jumpVelocity = 40f + level.mainSpeed / 10f;
-		grounded = false;
-		animator.SetBool("jumping", true);
-	}
+    void jump()
+    {
+        if (!grounded)
+        {
+            return;
+        }
+        stand();
+        if (jumpAudioSource && jumpAudioClip)
+        {
+            jumpAudioSource.PlayOneShot(jumpAudioClip, 1);
+        }
+        startVector = transform.position;
+        jumpVelocity = 40f + level.mainSpeed / 10f;
+        grounded = false;
+        animator.SetBool("jumping", true);
+    }
 
-	void duck() {
-		if (ducking || !grounded) {
-			return;
-		}
+    void duck()
+    {
+        if (ducking || !grounded)
+        {
+            return;
+        }
 
-		standingCollider.enabled = false;
-		duckingCollider.enabled = true;
-		ducking = true;
-		animator.SetBool("ducking", true);
-	}
+        standingCollider.enabled = false;
+        duckingCollider.enabled = true;
+        ducking = true;
+        animator.SetBool("ducking", true);
+    }
 
-	void invert() {
-		GameObject dinosaur = GameObject.Find("Dinosaur");
-		var x = dinosaur.transform.localScale.x;
-		var y = dinosaur.transform.localScale.y;
-		var z = dinosaur.transform.localScale.z;
-		dinosaur.transform.localScale = new Vector3(x, -y, z);
-	}
+    void invert()
+    {
+        GameObject dinosaur = GameObject.Find("Dinosaur");
+        var x = dinosaur.transform.localScale.x;
+        var y = dinosaur.transform.localScale.y;
+        var z = dinosaur.transform.localScale.z;
+        dinosaur.transform.localScale = new Vector3(x, -y, z);
+    }
 
-	void stand() {
-		if (!ducking) {
-			return;
-		}
+    void stand()
+    {
+        if (!ducking)
+        {
+            return;
+        }
 
-		standingCollider.enabled = true;
-		duckingCollider.enabled = false;
-		ducking = false;
-		animator.SetBool("ducking", false);
-	}
+        standingCollider.enabled = true;
+        duckingCollider.enabled = false;
+        ducking = false;
+        animator.SetBool("ducking", false);
+    }
 }
