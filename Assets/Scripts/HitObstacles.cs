10f9a9cb862327425a79a4e91c1e8d2d2ece235a
ArthurOx <oxenarthur@gmail.com>
2019-10-12 13:58:33 +0300

    added lives

--- Assets/Scripts/HitObstacles.cs
+++ Assets/Scripts/HitObstacles.cs
@@ -2,46 +2,82 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class HitObstacles : MonoBehaviour {
-	public AudioSource audioSource = null;
-	public AudioClip audioClip = null;
-	public GameObject restart = null;
-	private Animator animator;
-	public static bool stop = false;
-	private Vector3 restartStartPosition = Vector3.zero;
+public class HitObstacles : MonoBehaviour
+{
+    public AudioSource audioSource = null;
+    public AudioClip audioClip = null;
+    public GameObject restart = null;
+    private Animator animator;
+    public static bool stop = false;
+    private Vector3 restartStartPosition = Vector3.zero;
+    private int livesCount;
 
-	void Start() {
-		animator = GetComponent<Animator>();
-		restartStartPosition = restart.transform.position;
-		restart.transform.position = Vector3.up * 50;
-	}
+    void Start()
+    {
+        livesCount = 2;
+        animator = GetComponent<Animator>();
+        restartStartPosition = restart.transform.position;
+        restart.transform.position = Vector3.up * 50;
+    }
 
-	void Update() {
-		if (!stop || !Input.anyKeyDown) {
-			return;
-		}
-		SceneManager.LoadScene("Game");
-		Time.timeScale = 1;
-		SceneManager.sceneLoaded += OnGameRestarted;
-	}
+    void Update()
+    {
+        if (!stop || !Input.anyKeyDown)
+        {
+            return;
+        }
+        SceneManager.LoadScene("Game");
+        Time.timeScale = 1;
+        SceneManager.sceneLoaded += OnGameRestarted;
+    }
 
-	void OnGameRestarted(Scene scene, LoadSceneMode mode)
-	{
-		stop = false;
-	}
+    void OnGameRestarted(Scene scene, LoadSceneMode mode)
+    {
+        stop = false;
+    }
 
-	void OnTriggerEnter2D(Collider2D collider) {
-		if (audioSource && audioClip) {
-			audioSource.PlayOneShot(audioClip, 1);
-		}
-		restart.transform.position = restartStartPosition;
-		Time.timeScale = 0;
-		stop = true;
-		animator.SetTrigger("hit");
-		Animator otherAnimator = collider.gameObject.GetComponent<Animator>();
-		if (otherAnimator == null) {
-			return;
-		}
-		otherAnimator.SetTrigger("hit");
-	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        removeLife();
+        Debug.Log(livesCount);
+        if (livesCount == 0)
+        {
+            if (audioSource && audioClip)
+            {
+                audioSource.PlayOneShot(audioClip, 1);
+            }
+            restart.transform.position = restartStartPosition;
+            Time.timeScale = 0;
+            stop = true;
+            animator.SetTrigger("hit");
+            Animator otherAnimator = collider.gameObject.GetComponent<Animator>();
+            if (otherAnimator == null)
+            {
+                return;
+            }
+            otherAnimator.SetTrigger("hit");
+        }
+    }
+    void removeLife()
+    {
+        livesCount--;
+        changeHeartsSprite(livesCount);
+    }
+    void changeHeartsSprite(int numOfLives)
+    {
+        Object[] sprites = Resources.LoadAll<Sprite> ("heart sheet");
+        Debug.Log("sprlgth " + sprites.Length);
+        switch (numOfLives)
+        {
+            case 2:
+                GameObject.Find("Lives").GetComponent<SpriteRenderer>().sprite = (Sprite) sprites[2];
+                break;
+            case 1:
+                GameObject.Find("Lives").GetComponent<SpriteRenderer>().sprite = (Sprite) sprites[1];
+                break;
+            case 0:
+                GameObject.Find("Lives").GetComponent<SpriteRenderer>().sprite = (Sprite) sprites[0];
+                break;
+        }
+    }
 }
